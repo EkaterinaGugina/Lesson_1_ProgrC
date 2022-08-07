1a70791d71104e2fd515d2081665758cdd2c64b5
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-08 02:58:48 +0500

    Ex5 solved

--- Example5/Program.cs
+++ Example5/Program.cs
@@ -1,16 +1,38 @@
 ﻿// "сервировка стола" - рисунок из плюсиков
 Console.Clear();
-//Console.SetCursorPosition(10, 4);
-//Console.WriteLine("+");
-int xa = 1;
+int xa = 30;
 int ya = 1;
 int xb = 1;
-int yb = 30;
-int xc = 40;
-int yc = 30;
+int yb = 25;
+int xc = 60;
+int yc = 25;
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
 Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
+int x = xa; int y = ya;
+int count = 0;
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
