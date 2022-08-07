9f10d2852a260fb6096dce12a9468146b97b913c
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-07 20:13:19 +0500

    Ex3 solved

---
+++ Example3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите Ваше имя");
+
+string UserName = Console.ReadLine();
+
+if (UserName.ToLower() == "Маша")
+   {
+     Console.WriteLine("Ура! Это же Маша!");
+   }
+else
+   {
+     Console.Write("Привет, ");
+
+     Console.Write(UserName);
+
+     Console.Write("!");
+
+   }        
+       
