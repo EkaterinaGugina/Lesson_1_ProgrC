8ad2e9b0ac25d997314b37afec6cd9dd2d7ac8cd
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-08 00:46:07 +0500

    update Ex2 - выводим случайно сгенерированные числа на экран

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -17,8 +17,10 @@ Console.WriteLine($"Их частное равно: {(double)numberA / numberB}"
 Console.WriteLine("А теперь мы придумаем любые два числа и проделаем те же операции суммы и деления с ними:");
 
 int number1 = new Random().Next(1,100);
+Console.WriteLine(number1);
 
 int number2 = new Random().Next(1, 100);
+Console.WriteLine(number2);
 
 Console.WriteLine($"Их сумма равна: {number1 + number2}");
 
