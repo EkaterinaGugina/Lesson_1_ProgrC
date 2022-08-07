c265d9d4141fe72741debe9602dda932af434ca9
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-07 18:40:59 +0500

    update Ex1

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -5,3 +5,10 @@ Console.Write("Текущая дата и время:   ");
 
 Console.WriteLine(DateTime.Now);
 
+Console.WriteLine("Введите Ваше имя");
+
+string UserName = Console.ReadLine();
+
+Console.WriteLine("Привет, ");
+
+Console.WriteLine(UserName);
